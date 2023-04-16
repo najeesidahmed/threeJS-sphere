0a1d8077b6c6680c0d537875c3984d89e6ff237e
najee sidahmed <najeekamil@msn.com>
2023-04-17 00:00:38 +0100

    add existing files

---
+++ main.js
@@ -0,0 +1,77 @@
+import * as THREE from 'three';
+import './style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
+
+// Scene
+const scene = new THREE.Scene();
+
+// Create out sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+    color: '#00ff83',
+})
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
+
+// sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 5, 20)
+scene.add(light)
+
+//Camera 
+const camera = new THREE.PerspectiveCamera(
+  45, 
+  sizes.width / sizes.height, 
+  0.1, 
+  100
+)
+camera.position.z = 20
+scene.add(camera)
+
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene, camera);
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableSoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+// Resize
+window.addEventListener('resize', () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.heihgt = window.innerHeight;
+  // update camera
+  camera.updateProjectionMatrix()
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+})
+
+
+const loop = () => {
+  controls.update()
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+}
+loop();
+
+// timeline magic
+const tl = gsap.timeline({ defaults: { duration: 1 }});
+tl.fromTo(mesh.scale, { z:0, x:0, y:0 }, { z:1, x:1, y: 1 })
+
+
